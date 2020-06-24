c23443d9c595a2e427b39830036f6d60c44eac16
Lohrke <heiko.lohrke@ubirch.com>
2020-06-24 16:30:12 +0200

    First version

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # pycom-bootloader
-Simple bootloader for pycom devices to run code from SD instead of internal flash
+Simple bootloader for pycom devices to run code from SD instead of internal flash.
+Simply copy your project as-is to a subfolder called 'code' on the SD root.
+(So you end up with SD_root/code/main.py, SD_root/code/lib/yourlib.py, ... )
---
+++ src/main.py
@@ -0,0 +1,42 @@
+print("SD boot loader")
+
+import pycom
+import time
+import machine
+import sys
+import uos
+
+### Functions ###
+def mount_sd():
+    try:
+        sd = machine.SD()
+        os.mount(sd, '/sd')
+        return True
+    except OSError:
+        return False
+
+### Main Code ###
+pycom.heartbeat(False)
+pycom.rgbled(0x7f0000) # red        
+
+if mount_sd():
+    print("booting from SD")
+    pycom.rgbled(0x050505) # white
+    #add code and lib dir on sd to import path
+    sys.path.append('/sd/code')
+    sys.path.append('/sd/code/lib')
+    #change working dir to code directory
+    uos.chdir('/sd/code')
+    print("sys.path:")
+    print(sys.path)
+    print("uos.getcwd():")
+    print(uos.getcwd())
+    execfile('/sd/code/main.py')
+
+pycom.heartbeat(False)
+while True:
+    print("no SD found")
+    pycom.rgbled(0x7f7f00) # yellow
+    time.sleep(0.5)
+    pycom.rgbled(0x7f0000) # red
+    time.sleep(0.5)
